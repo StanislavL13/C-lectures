1051e13b236dacd9d5bca1b426eb871746599142
Stanislav Lobanov <s.lobanov13@gmail.com>
2022-01-29 17:42:23 +0700

    If Else

---
+++ Example005__IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Представьтесь, пожалуйста! ");
+string username = Console.ReadLine();
+if(username.ToLower() == "станислав")
+{
+    Console.WriteLine ("Здравствуй, Господин!");
+}
+else
+{
+    Console.WriteLine ("Добрый день, ");
+    Console.WriteLine(username);
+}
